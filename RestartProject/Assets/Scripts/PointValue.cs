875f21aa9a461eac8b7800a32c17269aeeeae1bb
ShayTruffitt <78729525+ShayTruffitt@users.noreply.github.com>
2021-02-22 15:54:19 +0000

    Monday afternoon

---
+++ RestartProject/Assets/Scripts/PointValue.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointValue : MonoBehaviour
+{
+    [SerializeField] float pointValue = 100f;
+
+    public void AddPoints()
+    {
+        if (GameManager.instance != null)
+        {
+            GameManager.instance.AddToScore(pointValue);
+        }
+    }
+}
